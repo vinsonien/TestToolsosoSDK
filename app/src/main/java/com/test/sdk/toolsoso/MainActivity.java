669cd25d28e98c620d153db4098d5284d9feb37b
vinson <vinsonhaveasecret@gmail.com>
2019-05-07 17:00:15 +0800

    添加教程代码

--- app/src/main/java/com/test/sdk/toolsoso/MainActivity.java
+++ app/src/main/java/com/test/sdk/toolsoso/MainActivity.java
@@ -1,13 +1,32 @@
 package com.test.sdk.toolsoso;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.btn)
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("tag","----------测试butterknife----------");
+            }
+        });
     }
 }
--- app/src/main/java/com/test/sdk/toolsoso/MyApp.java
+++ app/src/main/java/com/test/sdk/toolsoso/MyApp.java
@@ -1,6 +1,19 @@
 package com.test.sdk.toolsoso;
 
+import android.content.Context;
 import android.support.multidex.MultiDexApplication;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.blankj.utilcode.util.Utils;
+import com.facebook.stetho.Stetho;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.PrettyFormatStrategy;
+import com.squareup.leakcanary.RefWatcher;
+import com.vs.toolsoso.bugly.BuglyCrash;
+import com.vs.toolsoso.leakcanary.LeakManager;
+import com.vs.toolsoso.utils.LogUtil;
+
+import org.xutils.x;
 
 /**
  * @author: S
@@ -11,5 +24,78 @@ public class MyApp extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+
+    /**
+     * 内存泄漏检测
+     */
+    private RefWatcher refWatcher;
+    public static RefWatcher getRefWatcher(Context context) {
+        MyApp application = (MyApp) context.getApplicationContext();
+        return application.refWatcher;
+    }
+    private void InitLeakCanary() {
+        refWatcher = LeakManager.Init(this);
+    }
+
+    /**
+     * Blankj 常用工具
+     */
+    private void InitBlankjUtils() {
+        Utils.init(this);
+    }
+
+
+    /**
+     * 初始化 Utils
+     */
+    private void InitXUtils(boolean b) {
+        x.Ext.init(this);
+        x.Ext.setDebug(b); // 是否输出debug日志, 开启debug会影响性能.
+    }
+
+
+    /**
+     * 初始化路由sdk
+     */
+    private void InitARouter(boolean b) {
+        if (b) {
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(this); // 尽可能早，推荐在Application中初始化
+    }
+
+
+    /**
+     * 日志库
+     */
+    private void InitLoggUtil(boolean isDebug) {
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(false)  //是否选择显示线程信息，默认为true
+                .methodCount(0)         //方法数显示多少行，默认2行
+                .methodOffset(7)        //隐藏方法内部调用到偏移量，默认5
+                .tag("填写自己的TAG标签")   //自定义TAG全部标签，默认PRETTY_LOGGER
+                .build();
+        LogUtil.Init(formatStrategy, isDebug);
+    }
+
+    /**
+     * bugly
+     */
+    private void InitCrashReport() {
+        String buglyId = "平台里申请的appid";
+        String CrashChannel = BuglyCrash.DebugChannel;
+        BuglyCrash.InitCrash(this, buglyId, CrashChannel, true);
+    }
+
+
+    /**
+     * Stetho
+     */
+    private void InitStetho(boolean isDebug) {
+        if (isDebug) {
+            Stetho.initializeWithDefaults(this);
+        }
     }
 }
