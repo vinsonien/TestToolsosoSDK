291c272333219d3c7a11f000fb621d7e0b7b7f21
vinson <vinsonhaveasecret@gmail.com>
2019-05-15 11:59:36 +0800

    device工具类创建适配各系统版本的获取sn方法

--- toolsoso/src/main/java/com/vs/toolsoso/utils/DeviceUtil.java
+++ toolsoso/src/main/java/com/vs/toolsoso/utils/DeviceUtil.java
@@ -1,7 +1,13 @@
 package com.vs.toolsoso.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.blankj.utilcode.util.StringUtils;
 
 import java.lang.reflect.Method;
 
@@ -18,13 +24,40 @@ import java.lang.reflect.Method;
 public class DeviceUtil {
 
     /**
+     * 获取sn号  适配 9.0、8.0~4.0、4.0-
+     * @return
+     */
+    @SuppressLint({"NewApi", "MissingPermission"})
+    public static String GetDeviceSN() {
+
+        String serial = "";
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {//9.0+
+                serial = Build.getSerial();
+            } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {//8.0+
+                serial = Build.SERIAL;
+            } else {//8.0-
+                Class<?> c = Class.forName("android.os.SystemProperties");
+                Method get = c.getMethod("get", String.class);
+                serial = (String) get.invoke(c, "ro.serialno");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("TAG", "读取设备序列号异常：" + e.toString());
+        }
+        return serial;
+    }
+
+    /**
      * 是否是华为
      */
     public static boolean isHUAWEI() {
         return android.os.Build.MANUFACTURER.equals("HUAWEI");
     }
 
-    //获取是否存在NavigationBar
+    /**
+     *     获取是否存在NavigationBar
+     */
     public static boolean checkDeviceHasNavigationBar(Context context) {
         boolean hasNavigationBar = false;
         try {
@@ -45,5 +78,16 @@ public class DeviceUtil {
 
         }
         return hasNavigationBar;
+    }
+
+
+    @SuppressLint("MissingPermission")
+    public static String GetDeviceId(Context c){
+        TelephonyManager TelephonyMgr = (TelephonyManager)c.getSystemService(Context.TELEPHONY_SERVICE);
+        if(TelephonyMgr==null || TelephonyMgr.getDeviceId()==null || StringUtils.isEmpty(TelephonyMgr.getDeviceId())){
+            return "0000000000";
+        }else{
+            return TelephonyMgr.getDeviceId();
+        }
     }
 }
