e29fe6dc5799a8541f90572ef55371ee9096654e
liangweisen <vinsonien@163.com>
2020-06-22 11:00:30 +0800

    得到几天后的日期，添加格式适配参数

--- toolsoso/src/main/java/com/vs/toolsoso/utils/DateTimeUtil.java
+++ toolsoso/src/main/java/com/vs/toolsoso/utils/DateTimeUtil.java
@@ -393,8 +393,14 @@ public class DateTimeUtil {
 	 * @param day
 	 * @return
 	 */
-	public static String getDateAfter(int day) {
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+	public static String getDateAfter(int day,String timeformat) {
+		SimpleDateFormat formatter;
+		if(timeformat!=null&& !timeformat.equals("")){
+			formatter = new SimpleDateFormat(timeformat);
+		}else{
+			formatter = new SimpleDateFormat("yyyy-MM-dd");
+		}
+
 		Date curDate = new Date(System.currentTimeMillis());// 获取当前时间
 		Calendar now = Calendar.getInstance();
 		now.setTime(curDate);
